ed2dc506f97c9a2b6a6503f56f6953c4d3ac230e
SERTÃO ENGENHARIA <sertao.engenharia.ba@gmail.com>
2025-05-15 21:42:58 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('Site da Sertão Engenharia carregado com sucesso.');
+
+let index = 0;
+carousel();
+
+function carousel() {
+  let i;
+  let images = document.querySelectorAll(".carousel img");
+  for (i = 0; i < images.length; i++) {
+    images[i].classList.remove("active");
+  }
+  index++;
+  if (index > images.length) { index = 1; }
+  images[index - 1].classList.add("active");
+  setTimeout(carousel, 4000);
+}
